cf17d391a9bdc898e2f375857c8e49d5d18f0a1b
airstarh <vsevolod.azovsky@gmail.com>
2019-01-27 16:42:52 +0300

    dev

--- alinapoint/src/components/TableComponent/Tags/TagTrTh.vue
+++ alinapoint/src/components/TableComponent/Tags/TagTrTh.vue
@@ -4,9 +4,35 @@
 			Actions
 		</th>
 		<th v-for="(f, i) in refArrFieldsOrder">
-			<button @click="refArrCollection.arrFieldsOrderSetEarlier(i)"><</button>
-			{{f}}
-			<button @click="refArrCollection.arrFieldsOrderSetLater(i)">></button>
+			<div class="flex-menu">
+				<div class="flex-menu-item">
+					<button @click="refArrCollection.arrFieldsOrderSetEarlier(i)"><</button>
+				</div>
+				<div class="flex-menu-item">
+					{{f}}
+					<button>Sort</button>
+				</div>
+				<div class="flex-menu-item">
+					<button @click="refArrCollection.arrFieldsOrderSetLater(i)">></button>
+				</div>
+			</div>
+			<div class="get-params">
+				<div class="get-params-each">
+					<input type="text" :placeholder="f">
+				</div>
+				<div class="get-params-each">
+					<input type="text" :placeholder="`eq_${f}`">
+				</div>
+				<div class="get-params-each">
+					<input type="text" :placeholder="`lk_${f}`">
+				</div>
+				<div class="get-params-each">
+					<input type="text" :placeholder="`gt_${f}`">
+				</div>
+				<div class="get-params-each">
+					<input type="text" :placeholder="`lt_${f}`">
+				</div>
+			</div>
 		</th>
 	</tr>
 </template>
@@ -20,7 +46,7 @@
 	export default {
 		name:       'TagTrTh',
 		props:      {
-			refArrCollection: GeneralCollection,
+			refArrCollection:  GeneralCollection,
 			refArrFieldsOrder: Array,
 		},
 		components: {
@@ -28,5 +54,10 @@
 			TagTh,
 			TagTd,
 		},
+		methods:{
+			onChangeGetParams(fName, fValue){
+
+			}
+		}
 	}
 </script>
